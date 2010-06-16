92871d8f942cb5db6ff29bde1fc40ea254525611
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-06-16 14:24:06 -0700

    Added a ChangeLogAnnotator implementation that turns "Change-Id:" marker into a hyperlink.

---
+++ gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/ChangeIdAnnotator.java
@@ -0,0 +1,45 @@
+package com.sonyericsson.hudson.plugins.gerrit.trigger;
+
+import com.sonyericsson.hudson.plugins.gerrit.trigger.config.IGerritHudsonTriggerConfig;
+import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.GerritTrigger;
+import hudson.Extension;
+import hudson.MarkupText;
+import hudson.MarkupText.SubText;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.scm.ChangeLogAnnotator;
+import hudson.scm.ChangeLogSet.Entry;
+import hudson.triggers.Trigger;
+
+import java.util.regex.Pattern;
+
+/**
+ * Turns "Change-ID: XXXX" into a hyperlink to Gerrit.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class ChangeIdAnnotator extends ChangeLogAnnotator {
+    @Override
+    public void annotate(AbstractBuild<?,?> build, Entry change, MarkupText text) {
+        IGerritHudsonTriggerConfig config = PluginImpl.getInstance().getConfig();
+        annotate(build.getProject(), text, config);
+    }
+
+    public void annotate(AbstractProject<?,?> project, MarkupText text, IGerritHudsonTriggerConfig config) {
+        for (SubText token : text.findTokens(CHANGE_ID)) {
+            if (!hasGerritTrigger(project))   return; // not configured with Gerrit
+
+            token.href(config.getGerritFrontEndUrl()+"r/"+token.getText());
+        }
+    }
+
+    private boolean hasGerritTrigger(AbstractProject<?, ?> project) {
+        for (Trigger t : project.getTriggers().values())
+            if (t instanceof GerritTrigger)
+                return true;
+        return false;
+    }
+
+    private static final Pattern CHANGE_ID = Pattern.compile("(?<=\\bChange-Id: )I[0-9a-fA-F]{40}\\b");
+}
