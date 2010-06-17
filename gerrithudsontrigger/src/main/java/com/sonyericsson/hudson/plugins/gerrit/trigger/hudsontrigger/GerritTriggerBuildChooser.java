c4f6d4218a2d5e87f059659db890e7d235496497
Andrew Bayer <abayer@digg.com>
2010-06-17 14:35:22 -0700

    checkstyle fixes

--- gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/GerritTriggerBuildChooser.java
+++ gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/GerritTriggerBuildChooser.java
@@ -3,28 +3,35 @@ package com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger;
 
 import hudson.Extension;
 import hudson.model.TaskListener;
-import hudson.model.AbstractProject;
-import hudson.model.Action;
 import hudson.model.Result;
-import hudson.plugins.git.*;
+import hudson.plugins.git.IGitAPI;
+import hudson.plugins.git.GitAPI;
+import hudson.plugins.git.GitException;
+import hudson.plugins.git.Revision;
+import hudson.plugins.git.Branch;
 import hudson.plugins.git.util.Build;
 import hudson.plugins.git.util.BuildData;
-import hudson.plugins.git.util.GitUtils;
 import hudson.plugins.git.util.BuildChooser;
 import hudson.plugins.git.util.BuildChooserDescriptor;
-import hudson.util.DescribableList;
-import org.joda.time.DateTime;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.spearce.jgit.lib.ObjectId;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
 
+/**
+ * Used by Hudson git plugin to determine the revision to build.
+ * @author Andrew Bayer
+ */
 public class GerritTriggerBuildChooser extends BuildChooser {
     private final String separator = "#";
 
+    /**
+     * Default constructor.
+     */
     @DataBoundConstructor
     public GerritTriggerBuildChooser() {
     }
@@ -32,26 +39,29 @@ public class GerritTriggerBuildChooser extends BuildChooser {
     /**
      * Determines which Revisions to build.
      *
-     * 
-     *
      * Doesn't care about branches.
-     * @throws IOException
-     * @throws GitException
+     * @param isPollCall whether this is being called from Git polling
+     * @param singleBranch The branch
+     * @param git The GitAPI object
+     * @param listener TaskListener for logging, etc
+     * @param data the historical BuildData object
+     * @return A Collection containing the new revision.
+     * @throws GitException in case of error
+     * @throws IOException In case of error
      */
     @Override
     public Collection<Revision> getCandidateRevisions(boolean isPollCall, String singleBranch,
                                                       IGitAPI git, TaskListener listener, BuildData data)
-            throws GitException, IOException {
+        throws GitException, IOException {
 
         try {
             ObjectId sha1 = git.revParse("FETCH_HEAD");
-            
+
             Revision revision = new Revision(sha1);
             revision.getBranches().add(new Branch(singleBranch, sha1));
 
             return Collections.singletonList(revision);
-        }
-        catch (GitException e) {
+        } catch (GitException e) {
             // branch does not exist, there is nothing to build
             return Collections.<Revision>emptyList();
         }
@@ -66,10 +76,10 @@ public class GerritTriggerBuildChooser extends BuildChooser {
         ObjectId parentSha1 = getFirstParent(ObjectId.toString(sha1), git);
         Revision parentRev = new Revision(parentSha1);
         parentRev.getBranches().add(new Branch(singleBranch, parentSha1));
-        
+
         int prevBuildNum = 0;
         Result r = null;
-        
+
         Build lastBuild = data.getLastBuildOfBranch(singleBranch);
         if (lastBuild != null) {
             prevBuildNum = lastBuild.getBuildNumber();
@@ -77,24 +87,40 @@ public class GerritTriggerBuildChooser extends BuildChooser {
         }
 
         Build newLastBuild = new Build(parentRev, prevBuildNum, r);
-        
+
         return newLastBuild;
     }
-        
+
+    /**
+     * Gets the top parent of the given revision.
+     *
+     * @param revName Revision
+     * @param git GitAPI object
+     * @return object id for parent
+     * @throws GitException In case of error in git call
+     */
     private ObjectId getFirstParent(String revName, IGitAPI git) throws GitException {
         String result = ((GitAPI)git).launchCommand("log", "-1", "--pretty=format:%P", revName);
         return ObjectId.fromString(firstLine(result).trim());
     }
 
+    /**
+     * Gets the first line of the string.
+     *
+     * @param result String to get first line of
+     * @return first line of string
+     */
     private String firstLine(String result) {
         BufferedReader reader = new BufferedReader(new StringReader(result));
         String line;
         try {
             line = reader.readLine();
-            if (line == null)
+            if (line == null) {
                 return null;
-            if (reader.readLine() != null)
+            }
+            if (reader.readLine() != null) {
                 throw new GitException("Result has multiple lines");
+            }
         } catch (IOException e) {
             throw new GitException("Error parsing result", e);
         }
@@ -102,6 +128,9 @@ public class GerritTriggerBuildChooser extends BuildChooser {
         return line;
     }
 
+    /**
+     * Descriptor for GerritTriggerBuildChooser.
+     */
     @Extension
     public static final class DescriptorImpl extends BuildChooserDescriptor {
         @Override
