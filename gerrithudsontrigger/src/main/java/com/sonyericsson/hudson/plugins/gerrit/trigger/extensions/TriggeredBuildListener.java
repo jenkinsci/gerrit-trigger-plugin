61732be16d0b0dc449cf6fe964886feb0401acc2
rinrinne <rinrin.ne@gmail.com>
2013-08-19 19:56:34 +0900

    Add new interface to listen notification.
    To get notification for voting, new interface and utility class are
    added. Other plugin developer can listen notification by implementing
    this interface then put it to extension.

---
+++ gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/extensions/TriggeredBuildListener.java
@@ -0,0 +1,55 @@
+/*
+ *  The MIT License
+ *
+ *  Copyright 2010 Sony Ericsson Mobile Communications. All rights reserved.
+ *  Copyright 2012 Sony Mobile Communications AB. All rights reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package com.sonyericsson.hudson.plugins.gerrit.trigger.extensions;
+
+import hudson.model.Result;
+
+import com.sonyericsson.hudson.plugins.gerrit.gerritevents.dto.events.GerritTriggeredEvent;
+
+/**
+ * A listener for build result.
+ * Note that this interface should be implemented to extension.
+ *
+ * @author rinrinne &lt;rinrin.ne@gmail.com&gt;
+ */
+public interface TriggeredBuildListener {
+
+    /**
+     * Calls when build started.
+     *
+     * @param event the event.
+     * @param command the command.
+     */
+    void onStarted(GerritTriggeredEvent event, String command);
+
+    /**
+     * Calls when all builds completed.
+     *
+     * @param result the result.
+     * @param event the event.
+     * @param command the command.
+     */
+    void onCompleted(Result result, GerritTriggeredEvent event, String command);
+}
