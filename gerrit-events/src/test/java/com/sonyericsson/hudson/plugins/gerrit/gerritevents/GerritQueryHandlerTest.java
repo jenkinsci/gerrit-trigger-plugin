6262f2af37325818f45748856a37c607acedaf25
Slawomir Jaranowski <s.jaranowski+gh@gmail.com>
2013-10-14 11:14:36 +0200

    Unit test for class GerritQueryHandler
    Change-Id: I65e503f130a524083bd05edbcfac8a808860510b

---
+++ gerrit-events/src/test/java/com/sonyericsson/hudson/plugins/gerrit/gerritevents/GerritQueryHandlerTest.java
@@ -0,0 +1,155 @@
+package com.sonyericsson.hudson.plugins.gerrit.gerritevents;
+
+import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.Authentication;
+import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnection;
+import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnectionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link GerritQueryHandler}.
+ *
+ * @author : slawomir.jaranowski
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SshConnectionFactory.class)
+public class GerritQueryHandlerTest {
+
+    private GerritQueryHandler queryHandler;
+
+    private SshConnection sshConnectionMock;
+
+    /**
+     * Prepare moc for sshConnection and create GerritQueryHandler for test.
+     *
+     * @throws Exception when something wrong.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        sshConnectionMock = mock(SshConnection.class);
+
+        PowerMockito.mockStatic(SshConnectionFactory.class);
+        PowerMockito.doReturn(sshConnectionMock).when(SshConnectionFactory.class, "getConnection",
+                isA(String.class), isA(Integer.class), isA(String.class), isA(Authentication.class));
+
+
+        when(sshConnectionMock.executeCommandReader(anyString())).thenAnswer(new Answer<Reader>() {
+
+            @Override
+            public Reader answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return new StringReader("{\"project\":\"test\"}");
+            }
+        });
+
+        queryHandler = new GerritQueryHandler("", 0, "", new Authentication(null, ""));
+    }
+
+    /**
+     * Test {@Link GerritQueryHandler.queryJava} and {@Link GerritQueryHandler.queryJson}
+     * with only one parameter.
+     *
+     * @throws Exception when something wrong.
+     */
+
+    @Test
+    public void testQueryStr() throws Exception {
+
+        queryHandler.queryJava("X");
+        queryHandler.queryJson("X");
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --patch-sets --current-patch-set \"X\"");
+
+    }
+
+    /**
+     * Test {@Link GerritQueryHandler.queryJava} and {@Link GerritQueryHandler.queryJson}
+     * with full parameter list.
+     *
+     * @throws Exception when something wrong.
+     */
+    @Test
+    public void testQueryStr3Bool() throws Exception {
+
+        queryHandler.queryJava("X", true, true, true);
+        queryHandler.queryJson("X", true, true, true);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --patch-sets --current-patch-set --files \"X\"");
+
+        queryHandler.queryJava("X", true, true, false);
+        queryHandler.queryJson("X", true, true, false);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --patch-sets --current-patch-set \"X\"");
+
+        queryHandler.queryJava("X", true, false, true);
+        queryHandler.queryJson("X", true, false, true);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --patch-sets --files \"X\"");
+
+        queryHandler.queryJava("X", true, false, false);
+        queryHandler.queryJson("X", true, false, false);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --patch-sets \"X\"");
+
+        queryHandler.queryJava("X", false, true, true);
+        queryHandler.queryJson("X", false, true, true);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --current-patch-set --files \"X\"");
+
+        queryHandler.queryJava("X", false, true, false);
+        queryHandler.queryJson("X", false, true, false);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --current-patch-set \"X\"");
+
+        queryHandler.queryJava("X", false, false, true);
+        queryHandler.queryJson("X", false, false, true);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON --files \"X\"");
+
+        queryHandler.queryJava("X", false, false, false);
+        queryHandler.queryJson("X", false, false, false);
+
+        verify(sshConnectionMock, times(2))
+                .executeCommandReader("gerrit query --format=JSON \"X\"");
+
+    }
+
+    /**
+     * Test {@Link GerritQueryHandler.queryFiles}.
+     *
+     * @throws Exception when something wrong.
+     */
+    @Test
+    public void testQueryFiles() throws Exception {
+
+        queryHandler.queryFiles("X");
+
+        verify(sshConnectionMock)
+                .executeCommandReader("gerrit query --format=JSON --current-patch-set --files \"X\"");
+
+    }
+}
