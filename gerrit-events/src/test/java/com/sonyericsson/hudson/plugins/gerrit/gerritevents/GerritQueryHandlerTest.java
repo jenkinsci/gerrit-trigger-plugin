5df0dfb4eb4ebe66b4bb973907634835b8d320c8
Slawomir Jaranowski <s.jaranowski+gh@gmail.com>
2013-10-17 08:44:42 +0200

    Added missing copyright header - correct.
    Change-Id: Ic00e2040cf72ba21b80f8ea7ba5a50a836e2992d

--- gerrit-events/src/test/java/com/sonyericsson/hudson/plugins/gerrit/gerritevents/GerritQueryHandlerTest.java
+++ gerrit-events/src/test/java/com/sonyericsson/hudson/plugins/gerrit/gerritevents/GerritQueryHandlerTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2011 Sony Ericsson Mobile Communications. All rights reserved.
+ * Copyright 2013 Slawomir Jaranowski. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
