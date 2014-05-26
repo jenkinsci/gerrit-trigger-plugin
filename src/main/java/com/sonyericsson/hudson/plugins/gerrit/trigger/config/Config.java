6ed012c30bd87de86e71f9bece38126c6277e4ff
rinrinne <rinrin.ne@gmail.com>
2014-05-26 19:08:43 +0900

    Copy REST API config
    This patch copies config for REST API when create new server by copying
    existing server.
    
    Fix for JENKINS-23189

--- src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/config/Config.java
+++ src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/config/Config.java
@@ -200,6 +200,9 @@ public class Config implements IGerritHudsonTriggerConfig {
         notificationLevel = config.getNotificationLevel();
         gerritAuthKeyFile = new File(config.getGerritAuthKeyFile().getPath());
         gerritAuthKeyFilePassword = config.getGerritAuthKeyFilePassword();
+        useRestApi = config.isUseRestApi();
+        gerritHttpUserName = config.getGerritHttpUserName();
+        gerritHttpPassword = Secret.fromString(config.getGerritHttpPassword());
         gerritBuildCurrentPatchesOnly = config.isGerritBuildCurrentPatchesOnly();
         numberOfWorkerThreads = config.getNumberOfReceivingWorkerThreads();
         numberOfSendingWorkerThreads = config.getNumberOfSendingWorkerThreads();
