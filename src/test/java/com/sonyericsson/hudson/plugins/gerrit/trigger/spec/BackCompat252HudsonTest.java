2ac5b664801a2783fa0c11390fa350076fd9debd
Robert Sandell <rsandell@cloudbees.com>
2015-04-24 15:11:12 +0200

    Converting to JenkinsRule might make BackCompat252 less flaky

--- src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/spec/BackCompat252HudsonTest.java
+++ src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/spec/BackCompat252HudsonTest.java
@@ -23,10 +23,6 @@
  */
 package com.sonyericsson.hudson.plugins.gerrit.trigger.spec;
 
-import com.sonymobile.tools.gerrit.gerritevents.dto.GerritEventType;
-import com.sonymobile.tools.gerrit.gerritevents.dto.events.DraftPublished;
-import com.sonymobile.tools.gerrit.gerritevents.dto.events.GerritTriggeredEvent;
-import com.sonymobile.tools.gerrit.gerritevents.dto.events.PatchsetCreated;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.PluginImpl;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.BadgeAction;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.GerritManualCause;
@@ -37,46 +33,67 @@ import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.GerritP
 import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.events.PluginGerritEvent;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.mock.Setup;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.mock.TestUtils;
+import com.sonymobile.tools.gerrit.gerritevents.dto.GerritEventType;
+import com.sonymobile.tools.gerrit.gerritevents.dto.events.DraftPublished;
+import com.sonymobile.tools.gerrit.gerritevents.dto.events.GerritTriggeredEvent;
+import com.sonymobile.tools.gerrit.gerritevents.dto.events.PatchsetCreated;
 import com.sonymobile.tools.gerrit.gerritevents.mock.SshdServerMock;
-
 import hudson.matrix.MatrixBuild;
 import hudson.matrix.MatrixProject;
 import hudson.model.AbstractProject;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
-import hudson.model.Hudson;
 import hudson.model.Item;
 import hudson.model.Result;
 import hudson.model.TopLevelItem;
 import hudson.util.RunList;
-
 import org.apache.sshd.SshServer;
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests to see if jobs from Gerrit-Trigger v. 2.5.2 can be loaded.
  *
  * @author Robert Sandell &lt;robert.sandell@sonyericsson.com&gt;
  */
-public class BackCompat252HudsonTest extends HudsonTestCase {
+public class BackCompat252HudsonTest {
 
     /**
      * The stream-events command.
      */
     protected static final String GERRIT_STREAM_EVENTS = "gerrit stream-events";
+    /**
+     * An instance of Jenkins Rule.
+     */
+    // CS IGNORE VisibilityModifier FOR NEXT 2 LINES. REASON: JenkinsRule.
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
     private SshServer sshd;
     private SshdServerMock.KeyPairFiles sshKey;
     private SshdServerMock server;
 
-    @Override
-    protected void setUp() throws Exception {
+    /**
+     * Starts up the sshd server.
+     *
+     * @throws Exception if so
+     */
+    @Before
+    public void setUp() throws Exception {
         sshKey = SshdServerMock.generateKeyPair();
         server = new SshdServerMock();
         sshd = SshdServerMock.startServer(server);
@@ -85,22 +102,27 @@ public class BackCompat252HudsonTest extends HudsonTestCase {
         server.returnCommandFor("gerrit review.*", SshdServerMock.EofCommandMock.class);
         server.returnCommandFor("gerrit version", SshdServerMock.EofCommandMock.class);
         System.setProperty(PluginImpl.TEST_SSH_KEYFILE_LOCATION_PROPERTY, sshKey.getPrivateKey().getAbsolutePath());
-        super.setUp();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    /**
+     * Shuts down the sshd server.
+     *
+     * @throws Exception if so
+     */
+    @After
+    public void tearDown() throws Exception {
         sshd.stop(true);
         sshd = null;
+        server = null;
     }
 
     /**
      * Tests that the FreeStyleProject can be loaded.
      */
+    @Test
     @LocalData
     public void testFreeStyleJob() {
-        TopLevelItem freestyleJobItem = Hudson.getInstance().getItem("freestyleJob");
+        TopLevelItem freestyleJobItem = j.jenkins.getItem("freestyleJob");
         assertThat(freestyleJobItem, instanceOf(AbstractProject.class));
         AbstractProject freestyleJob = (AbstractProject)freestyleJobItem;
         assertNotNull(freestyleJob);
@@ -117,16 +139,17 @@ public class BackCompat252HudsonTest extends HudsonTestCase {
         assertThat(branch.getPattern(), equalTo("bra.*"));
         List<PluginGerritEvent> triggerOnEvents = trigger.getTriggerOnEvents();
         assertThat(triggerOnEvents.size(), equalTo(2));
-        assertEquals(triggerOnEvents.get(0).getCorrespondingEventClass(), PatchsetCreated.class);
-        assertEquals(triggerOnEvents.get(1).getCorrespondingEventClass(), DraftPublished.class);
+        assertSame(triggerOnEvents.get(0).getCorrespondingEventClass(), PatchsetCreated.class);
+        assertSame(triggerOnEvents.get(1).getCorrespondingEventClass(), DraftPublished.class);
     }
 
     /**
      * Tests that the MatrixProject can be loaded.
      */
+    @Test
     @LocalData
     public void testMatrixJob() {
-        TopLevelItem matrixJobItem = Hudson.getInstance().getItem("matrixJob");
+        TopLevelItem matrixJobItem = j.jenkins.getItem("matrixJob");
         assertThat(matrixJobItem, instanceOf(AbstractProject.class));
         AbstractProject matrixJob = (AbstractProject)matrixJobItem;
         assertNotNull(matrixJob);
@@ -143,21 +166,22 @@ public class BackCompat252HudsonTest extends HudsonTestCase {
         assertThat(branch.getPattern(), equalTo(".*er"));
         List<PluginGerritEvent> triggerOnEvents = trigger.getTriggerOnEvents();
         assertThat(triggerOnEvents.size(), equalTo(2));
-        assertEquals(triggerOnEvents.get(0).getCorrespondingEventClass(), PatchsetCreated.class);
-        assertEquals(triggerOnEvents.get(1).getCorrespondingEventClass(), DraftPublished.class);
+        assertSame(triggerOnEvents.get(0).getCorrespondingEventClass(), PatchsetCreated.class);
+        assertSame(triggerOnEvents.get(1).getCorrespondingEventClass(), DraftPublished.class);
     }
 
     /**
      * Tests that the builds from the FreeStyleProject can be loaded.
      */
+    @Test
     @LocalData
     public void testFreeStyleBuild() {
-        Item item = Hudson.getInstance().getItem("freestyleJob");
+        Item item = j.jenkins.getItem("freestyleJob");
         assertThat("Item is not a FreeStyleProject", item, instanceOf(FreeStyleProject.class));
         FreeStyleProject proj = (FreeStyleProject)item;
         RunList<FreeStyleBuild> builds = proj.getBuilds();
         assertNotNull(builds);
-        assertTrue("The build list should not be empty", builds.size() > 0);
+        assertFalse("The build list should not be empty", builds.isEmpty());
         FreeStyleBuild freeStyleBuild = builds.get(0);
         assertNotNull(freeStyleBuild.getAction(RetriggerAction.class));
         GerritManualCause cause = freeStyleBuild.getCause(GerritManualCause.class);
@@ -167,15 +191,16 @@ public class BackCompat252HudsonTest extends HudsonTestCase {
         GerritTriggeredEvent event = action.getEvent();
         assertNotNull(event);
         GerritEventType eventType = event.getEventType();
-        assertEquals(eventType.getEventRepresentative(), PatchsetCreated.class);
+        assertSame(eventType.getEventRepresentative(), PatchsetCreated.class);
     }
 
     /**
      * Tests that the builds from the MatrixProject can be loaded.
      */
+    @Test
     @LocalData
     public void testMatrixBuild() {
-        Item item = Hudson.getInstance().getItem("matrixJob");
+        Item item = j.jenkins.getItem("matrixJob");
         assertThat("Item is not a MatrixProject", item, instanceOf(MatrixProject.class));
         MatrixProject proj = (MatrixProject)item;
         RunList<MatrixBuild> builds = proj.getBuilds();
@@ -190,17 +215,19 @@ public class BackCompat252HudsonTest extends HudsonTestCase {
     }
 
     //CS IGNORE MagicNumber FOR NEXT 20 LINES. REASON: Testdata
+
     /**
      * Tests that a new build can be triggered for the old FreeStyleProject.
      */
+    @Test
     @LocalData
     public void testNewTriggeredBuild() {
-        Item item = Hudson.getInstance().getItem("freestyleJob");
+        server.waitForCommand(GERRIT_STREAM_EVENTS, 10000);
+        Item item = j.jenkins.getItem("freestyleJob");
         assertThat("Item is not a FreeStyleProject", item, instanceOf(FreeStyleProject.class));
         FreeStyleProject project = (FreeStyleProject)item;
         int number = project.getLastBuild().getNumber() + 1;
-        server.waitForCommand(GERRIT_STREAM_EVENTS, 2000);
-        PluginImpl.getInstance().getServer(PluginImpl.DEFAULT_SERVER_NAME).triggerEvent(Setup.createPatchsetCreated());
+        PluginImpl.getServer_(PluginImpl.DEFAULT_SERVER_NAME).triggerEvent(Setup.createPatchsetCreated());
         TestUtils.waitForBuilds(project, number);
         //3 old builds + the new one.
         assertEquals(number, project.getLastCompletedBuild().getNumber());
