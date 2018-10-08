e82bdce82111e34b87db955298b851d843c0b6e9
cashlalala <cash924n@gmail.com>
2018-10-08 20:29:40 +0800

    fix GerritServerTest error

--- src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/GerritServerTest.java
+++ src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/GerritServerTest.java
@@ -155,7 +155,7 @@ public class GerritServerTest {
      */
     @Test
     public void testGetDisabledFeaturesNone() throws Exception {
-        String version = "2.13";
+        String version = "2.15";
         when(gerritServerOne.getGerritVersion()).thenReturn(version);
         listener.checkGerritVersionFeatures();
 
@@ -185,7 +185,7 @@ public class GerritServerTest {
      */
     @Test
     public void testHasDisabledFeaturesNot() throws Exception {
-        String version = "2.13";
+        String version = "2.15";
         when(gerritServerOne.getGerritVersion()).thenReturn(version);
         listener.checkGerritVersionFeatures();
 
