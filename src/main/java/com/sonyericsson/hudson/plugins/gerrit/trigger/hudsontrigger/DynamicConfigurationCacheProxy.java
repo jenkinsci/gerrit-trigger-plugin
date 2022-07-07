a080a86a9fe3d6cb0f4c7becb74ad960a08ba2cf
ckreisl <christoph.kreisl@gmx.de>
2022-07-07 20:19:59 +0200

    Fix map exception
    Item is removed in for loop which is unsave.
    Refactored code to use iterator.
    Introduced new helper classes for readability.

--- src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/DynamicConfigurationCacheProxy.java
+++ src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/DynamicConfigurationCacheProxy.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Iterator;
 
 /**
  * Keeps map between url and dynamic trigger configuration.
@@ -17,10 +18,149 @@ import java.util.Map;
  * trigger jobs and reduce number of connections for the duplicated configs.
  */
 final class DynamicConfigurationCacheProxy {
+
+    private static final Logger logger = LoggerFactory.getLogger(
+        DynamicConfigurationCacheProxy.class);
+
     private static final DynamicConfigurationCacheProxy CACHE_PROXY = new DynamicConfigurationCacheProxy();
-    private final Map<String, List<GerritProject>> cache = new HashMap<String, List<GerritProject>>();
-    private static final Logger logger = LoggerFactory.getLogger(DynamicConfigurationCacheProxy.class);
-    private final Map<String, Long> ttl = new HashMap<String, Long>();
+    private final Cache cache = new Cache();
+
+    /**
+     * A cache entry for the simplistic cache implementation
+     */
+    class CacheEntry {
+        private final long created;
+        private final List<GerritProject> projects;
+
+        /**
+         * Creates a cache entry
+         *
+         * @param projects
+         *   the project list from gerrit
+         */
+        public CacheEntry(List<GerritProject> projects) {
+            this.created = System.currentTimeMillis();
+            this.projects = projects;
+        }
+
+        /**
+         * Checks if the cache entry is expired.
+         *
+         * @param ttl
+         *   the timespan after which entries are regarded as expired.
+         * @return
+         *   true in case the entry is expired otherwise false.
+         */
+        public boolean isExpired(long ttl) {
+            return (ttl > created);
+        }
+
+        /**
+         * Returns the project list for this entry.
+         *
+         * @return
+         *   the cached project list.
+         */
+        public List<GerritProject> getProjects() {
+            return this.projects;
+        }
+    }
+
+    /**
+     * A very simplistic cache implementation.
+     * The cleanup method needs to called on a regular basis.
+     */
+    class Cache {
+        private final Map<String, CacheEntry> cache = new HashMap<>();
+
+        /**
+         * Creates a new instance
+         */
+        public Cache() {
+        }
+
+        /**
+         * Gets the entry list for the given url from the cache.
+         * Will return a null in case the url is unknown or the entry
+         * is too old/expired.
+         *
+         * @param url
+         *   the url from which the project list was fetched
+         * @param ttl
+         *   the timestamp after which the entry is regarded as too old.
+         *
+         * @return
+         *   the cached project list or null in case the cache does not
+         *   know the url or the cache entry has expired.
+         */
+        public List<GerritProject> get(String url, long ttl) {
+            synchronized (this.cache) {
+                CacheEntry entry = cache.get(url);
+
+                if (entry == null) {
+                    return null;
+                }
+
+                if (entry.isExpired(ttl)) {
+                    return null;
+                }
+
+                return entry.getProjects();
+            }
+        }
+
+        /**
+         * Adds a list of gerrit projects to the cache.
+         *
+         * @param url
+         *   the url from which the project list was fetched.
+         * @param projects
+         *   the list with projects returned by gerrit.
+         *
+         * @return
+         *   the project list which was added to the cache.
+         */
+        public List<GerritProject> put(String url, List<GerritProject> projects) {
+            synchronized (this.cache) {
+                this.cache.put(url, new CacheEntry(projects));
+            }
+
+            return projects;
+        }
+
+        /**
+         * Cleans the cache, needs to be called on a regular base.
+         *
+         * It will remove all entries which are older than the ttl.
+         * @param ttl
+         *   the timestamp after which entries are regarded as expired.
+         */
+        public void cleanup(long ttl) {
+            synchronized (this.cache) {
+                Iterator<Map.Entry<String, CacheEntry> > it = this.cache.entrySet().iterator();
+
+                while (it.hasNext()) {
+                    Map.Entry<String, CacheEntry> entry = it.next();
+
+                    if (!entry.getValue().isExpired(ttl)) {
+                        continue;
+                    }
+
+                    logger.trace("Removing expired url {} from cache", entry.getKey());
+                    it.remove();
+                }
+            }
+        }
+
+        /**
+         * Flushes the cache and removes all entries.
+         */
+        public void clear() {
+            synchronized (this.cache) {
+                this.cache.clear();
+            }
+        }
+    }
 
     /**
      * Private constructor.
@@ -29,35 +169,39 @@ final class DynamicConfigurationCacheProxy {
     }
 
     /**
+     * Calculates the timestamp when an entry is regarded to old and should
+     * be discarded.
+     *
+     * @return the maximal age.
+     */
+    public long getMaxTTL() {
+        long updateInterval = TimeUnit.SECONDS.toMillis(
+            GerritTriggerTimer.getInstance().calculateAverageDynamicConfigRefreshInterval());
+        return System.currentTimeMillis() - updateInterval;
+    }
+
+    /**
      * Returns dynamic trigger config from the cache if it's available.
      * Otherwise send query.
      *
      * @param url url to dynamic trigger config.
      * @return list of gerrit projects.
-     * @throws IOException if so.
+     * @throws IOException    if so.
      * @throws ParseException if so.
      */
-    synchronized List<GerritProject> fetchThroughCache(String url) throws IOException, ParseException {
-        if (cache.containsKey(url) && !isExpired(url)) {
+
+    public List<GerritProject> fetchThroughCache(String url) throws IOException, ParseException {
+        long ttl = this.getMaxTTL();
+        List<GerritProject> projects = this.cache.get(url, ttl);
+
+        if (projects != null) {
+            // Use the idle time to do cleanup.
             logger.debug("Get dynamic projects from cache for URL: " + url);
-            // Maintain cache while not fetching from URL
-            for (String keyUrl : ttl.keySet()) {
-               if (isExpired(keyUrl)) {
-                   ttl.remove(keyUrl);
-                   cache.remove(keyUrl);
-                   logger.trace("Removing {} from cache", keyUrl);
-               }
-            }
-
-            return cache.get(url);
+            this.cache.cleanup(ttl);
+            return projects;
         }
-
         logger.info("Get dynamic projects directly for URL: {}", url);
-        List<GerritProject> gerritProjects = GerritDynamicUrlProcessor.fetch(url);
-        ttl.put(url, System.currentTimeMillis());
-        cache.put(url, gerritProjects);
-
-        return gerritProjects;
+        return this.cache.put(url, GerritDynamicUrlProcessor.fetch(url));
     }
 
     /**
@@ -65,31 +209,14 @@ final class DynamicConfigurationCacheProxy {
      *
      * @return cache proxy object.
      */
-    static DynamicConfigurationCacheProxy getInstance() {
+    public static DynamicConfigurationCacheProxy getInstance() {
         return CACHE_PROXY;
-    }
-
-    /**
-     * Check the need to update specified url.
-     *
-     * @param url url.
-     * @return true if cached value is expired.
-     */
-    private boolean isExpired(String url) {
-        Long lastTimeUpdated = ttl.get(url);
-        if (lastTimeUpdated == null) {
-            lastTimeUpdated = System.currentTimeMillis();
-        }
-
-        long updateInterval = GerritTriggerTimer.getInstance().calculateAverageDynamicConfigRefreshInterval();
-        return TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - lastTimeUpdated) > updateInterval;
     }
 
     /**
      * Clears the cache.
      */
     void clear() {
-        ttl.clear();
-        cache.clear();
+        this.cache.clear();
     }
 }
--- src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/DynamicConfigurationCacheProxyTest.java
+++ src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/DynamicConfigurationCacheProxyTest.java
@@ -7,13 +7,17 @@ import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -50,16 +54,63 @@ public class DynamicConfigurationCacheProxyTest {
     }
 
     /**
+     * Test CacheEntry if entry is expired
+     */
+    @Test
+    public void testCacheEntryIsExpired() {
+        final long ttl = 1337;
+        final long created = 42;
+        List<GerritProject> projects = Collections.singletonList(mock(GerritProject.class));
+        DynamicConfigurationCacheProxy proxy = DynamicConfigurationCacheProxy.getInstance();
+        DynamicConfigurationCacheProxy.CacheEntry cacheEntry = proxy.new CacheEntry(projects);
+        Whitebox.setInternalState(cacheEntry, "created", created);
+        assertTrue(cacheEntry.isExpired(ttl));
+    }
+
+    /**
+     * Test CacheEntry if entry is not expired
+     */
+    @Test
+    public void testCacheEntryIsNotExpired() {
+        final long ttl = 42;
+        final long created = 1337;
+        List<GerritProject> projects = Collections.singletonList(mock(GerritProject.class));
+        DynamicConfigurationCacheProxy proxy = DynamicConfigurationCacheProxy.getInstance();
+        DynamicConfigurationCacheProxy.CacheEntry cacheEntry = proxy.new CacheEntry(projects);
+        Whitebox.setInternalState(cacheEntry, "created", created);
+        assertFalse(cacheEntry.isExpired(ttl));
+    }
+
+    /**
+     * Test CacheEntry getProjects
+     */
+    @Test
+    public void testCacheEntryGetProjects() {
+        List<GerritProject> projects = Collections.singletonList(mock(GerritProject.class));
+        DynamicConfigurationCacheProxy proxy = DynamicConfigurationCacheProxy.getInstance();
+        DynamicConfigurationCacheProxy.CacheEntry cacheEntry = proxy.new CacheEntry(projects);
+        assertEquals(projects, cacheEntry.getProjects());
+    }
+
+    /**
+     * Test Cache get item is empty and returns null
+     */
+    @Test
+    public void testCacheGetEmptyProjectList() {
+        DynamicConfigurationCacheProxy proxy = DynamicConfigurationCacheProxy.getInstance();
+        DynamicConfigurationCacheProxy.Cache cache = proxy.new Cache();
+        assertNull(cache.get(anyString(), anyLong()));
+    }
+
+    /**
      * Tests the case when cache is empty.
      * @throws Exception if so.
      */
     @Test
-    public void fetchDirectlyWithoutCache() throws Exception {
+    public void testFetchDirectlyWithoutCache() throws Exception {
         List<GerritProject> expected = Collections.singletonList(mock(GerritProject.class));
         when(GerritDynamicUrlProcessor.fetch(anyString())).thenReturn(expected);
-
         List<GerritProject> actual = DynamicConfigurationCacheProxy.getInstance().fetchThroughCache("someUrl");
-
         assertEquals(expected, actual);
     }
 
@@ -68,13 +119,13 @@ public class DynamicConfigurationCacheProxyTest {
      * @throws Exception if so.
      */
     @Test
-    public void fetchDirectlyWithCache() throws Exception {
+    public void testFetchDirectlyWithCache() throws Exception {
         List<GerritProject> gerritProjects1 = Collections.singletonList(mock(GerritProject.class));
         List<GerritProject> gerritProjects2 = Collections.singletonList(mock(GerritProject.class));
         when(GerritDynamicUrlProcessor.fetch(anyString())).thenReturn(gerritProjects1).thenReturn(gerritProjects2);
         setRefreshInternal(FORCE_REFRESH_INTERVAL);
 
-        List<GerritProject> res1 = DynamicConfigurationCacheProxy.getInstance().fetchThroughCache("someUrl");
+        List<GerritProject> res1 = DynamicConfigurationCacheProxy.getInstance().fetchThroughCache("debug");
         List<GerritProject> res2 = DynamicConfigurationCacheProxy.getInstance().fetchThroughCache("someUrl");
 
         assertNotEquals(res1, res2);
@@ -88,7 +139,7 @@ public class DynamicConfigurationCacheProxyTest {
      * @throws Exception if so.
      */
     @Test
-    public void fetchThroughCache() throws Exception {
+    public void testFetchThroughCache() throws Exception {
         PowerMockito.mockStatic(GerritDynamicUrlProcessor.class);
         List<GerritProject> gerritProjects1 = Collections.singletonList(mock(GerritProject.class));
         List<GerritProject> gerritProjects2 = Collections.singletonList(mock(GerritProject.class));
