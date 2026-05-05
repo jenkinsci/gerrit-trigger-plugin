131018f1be404cb7742af70c02d264e0aaebe186
Ignacio Roncero <ironcero@cloudbees.com>
2026-05-05 12:08:18 +0200

    PR review

--- src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/gerritnotifier/model/BuildMemory.java
+++ src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/gerritnotifier/model/BuildMemory.java
@@ -393,24 +393,29 @@ public class BuildMemory {
                 if (hasActiveBuildsForJob) {
                     outdatedEvents.add(runningChangeBasedEvent);
                     logger.debug("Added event to outdated list");
+                    // TODO:
+                    // We should consider adding the following in case we need to mark the builds in the
+                    // event as cancelled due to the BuildMemory is not only used by the cancellation policy.
+                    //
                     // NOTE: Do NOT remove the event from BuildMemory here!
                     // Unlike the old RunningJobs code (which was separate from lifecycle tracking),
                     // BuildMemory needs to keep tracking cancelled events for lifecycle/feedback.
                     // Mark the Entry as cancelled immediately so it won't be considered "active"
                     // in future cancellation checks (prevents state accumulation issues).
+                    //
                     // IMPORTANT: We need to get the imprint from storage again to modify the real one,
                     // not the copy from getAllEvents()
-                    MemoryImprint storageImprint = storage.getMemoryImprint(runningEvent);
-                    if (storageImprint != null) {
-                        for (Entry imprintEntry : storageImprint.getEntries()) {
-                            if (imprintEntry.isProject(jobName)
-                                    && !imprintEntry.isBuildCompleted()
-                                    && !imprintEntry.isCancelled()) {
-                                imprintEntry.setCancelled(true);
-                                imprintEntry.setBuildCompleted(true);
-                            }
-                        }
-                    }
+                    // MemoryImprint storageImprint = storage.getMemoryImprint(runningEvent);
+                    // if (storageImprint != null) {
+                    //     for (Entry imprintEntry : storageImprint.getEntries()) {
+                    //         if (imprintEntry.isProject(jobName)
+                    //                 && !imprintEntry.isBuildCompleted()
+                    //                 && !imprintEntry.isCancelled()) {
+                    //             imprintEntry.setCancelled(true);
+                    //             imprintEntry.setBuildCompleted(true);
+                    //         }
+                    //     }
+                    // }
                 }
             }
 
