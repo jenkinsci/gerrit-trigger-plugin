8ee3130782a2d40b066e62b559be230cf6baa1e6
Ignacio Roncero <ironcero@cloudbees.com>
2026-05-13 10:21:15 +0200

    Removing copyright modification

--- src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/EventListener.java
+++ src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/EventListener.java
@@ -2,7 +2,6 @@
  *  The MIT License
  *
  *  Copyright (c) 2014, CloudBees, Inc. All rights reserved.
- *  Copyright 2026 CloudBees, Inc.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
